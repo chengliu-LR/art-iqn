03f0c712b2cd39b70d84444c16ab1d040fad76d6
Cheng <c.liu-10@tudelft.nl>
2022-02-24 17:40:12 +0100

    tcv algorithm

--- tactical.py
+++ tactical.py
@@ -12,7 +12,7 @@ import numpy as np
 from collections import deque
 
 from agent import DQNAgent
-from utils.util import to_gym_interface_pos
+from utils.util import to_gym_interface_pos, ExpoWeightedAveForcast
 import matplotlib.pyplot as plt
 
 import crazyflie_env
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     parser.add_argument('--test_eps', default=0.0, type=float, help='Learning rate')
     parser.add_argument('--random_obstacle', default=1, type=int, help='Enable random obstacle generation or fixed obstacle position')
     parser.add_argument('--variance_samples_n', default=8, type=int, help='Truncated Variance calculation hyperparameter')
+    parser.add_argument('--tcv_lr', default=0.1, type=float, help='Updating rate for TCV controller')
     args = parser.parse_args()
 
     env = gym.make("CrazyflieEnv-v0")
@@ -56,11 +57,18 @@ if __name__ == "__main__":
     # if you want to set robot initial position by hand:
     env.robot.set_state(args.init_x, args.init_y, 0, 0, 0.0, 3.0, 0, 0, env.obstacle_segments) # generalize to a slightly modified goal position
     state = env.robot.observe()
+    tcv_controller = ExpoWeightedAveForcast(arms=[0.1, 1], lr=args.tcv_lr, init=0.01, use_std=True)
+
     print('initial state:', state) #observable state: px, py, vx, vy, radius
 
     state_size = len(to_gym_interface_pos(state))
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     eps=args.test_eps # exploration rate during test
+    logger_test = {}
+    logger_test['tcv'] = []
+    logger_test['cvar_probs'] = []
+    logger_test['adaptive_cvar'] = []
+    logger_test['feedback'] = []
 
     agent = DQNAgent(state_size=state_size,
                         num_directions=args.num_directions,
@@ -69,7 +77,7 @@ if __name__ == "__main__":
                         n_step=args.n_step,
                         BATCH_SIZE=args.batch_size,
                         BUFFER_SIZE=args.buffer_size,
-                        LR=args.lr, 
+                        LR=args.lr,
                         TAU=args.tau,
                         GAMMA=args.gamma,
                         UPDATE_EVERY=args.update_every,
@@ -86,25 +94,32 @@ if __name__ == "__main__":
 
     done = False
     score = 0
-    tcvs = []
+    tcv_window = deque(maxlen=3)
+    last_tcv = 0
     while not done:
         # pass tactical sampled cvar to agent.act()
         #cvar = np.random.uniform(0.1, 0.2)
         #action_id, action = agent.act(to_gym_interface_pos(state), eps, args.cvar)
-        action_id, action = agent.act(to_gym_interface_pos(state), eps, args.cvar)
+        cvar = tcv_controller.sample()
+        action_id, action = agent.act(to_gym_interface_pos(state), eps, cvar)
         next_state, reward, done, info = env.step(action)
         tcv = agent.get_tcv(to_gym_interface_pos(state), action_id)
         
-        
         # TODO: Tactical Algorithm
-
+        feedback = last_tcv - tcv
+        probs = tcv_controller.get_probs()
+        tcv_controller.update_dists(feedback)
 
         state = next_state
         score += reward
-        tcvs.append(tcv.item())
-        print("dist {}, reward {}, {}, vx {}, vy {}, tcv {}".format(round(state.goal_distance, 6), round(reward, 2), info, round(state.velocity[0], 2), round(state.velocity[1], 2), tcv.item()))
+        logger_test['tcv'].append(tcv.item())
+        logger_test['adaptive_cvar'].append(cvar)
+        logger_test['cvar_probs'].append(probs) # [0.1, 1]
+        logger_test['feedback'].append(feedback)
+        #print("dist {}, reward {}, {}, vx {}, vy {}, tcv {} probs {}".format(round(state.goal_distance, 6), round(reward, 2), info, round(state.velocity[0], 2), round(state.velocity[1], 2), tcv.item(), probs))
+        last_tcv = tcv
     print("Episodic return:", score, "Task time", env.global_time)
 
-    print([[round(obs.centroid[0], 2), round(obs.centroid[1], 2), round(obs.wx, 2), round(obs.wy, 2)] for obs in env.obstacles])
-    #env.render(mode=args.render_mode, output_file="./figures/iqn_random_init", density=args.render_density)
-    env.multi_render(mode=args.render_mode, output_file="./figures/iqn_random_init", tcvs=tcvs)
+    #print([[round(obs.centroid[0], 2), round(obs.centroid[1], 2), round(obs.wx, 2), round(obs.wy, 2)] for obs in env.obstacles])
+    env.multi_render(mode=args.render_mode, output_file="./figures/iqn_random_init", tcvs=logger_test['tcv'], cvars=logger_test['adaptive_cvar'])
+    pickle.dump(logger_test, open("experimentsCrazy/{}/logger_test.pkl".format(args.dir), 'wb'))
